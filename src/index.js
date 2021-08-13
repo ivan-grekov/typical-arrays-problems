684184b8c5f09adfed47c01c79fc071087e715a4
Ivan <ivan_grekov@yahoo.com>
2021-08-13 22:07:39 +0300

    feat: add func avg

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ exports.min = function min (array) {
   if (!Array.isArray(array) || array <= 0) {
     return 0;
   }
-    return Math.min(... array);
+  return Math.min(... array);
 }
 
 exports.max = function max (array) {
   if (!Array.isArray(array) || array <= 0) {
     return 0;
   }
-    return Math.max(... array);
+  return Math.max(... array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array <= 0) {
+    return 0;
+  }
+  return array.reduce((sum, item) => sum + item) / array.length;
 }
